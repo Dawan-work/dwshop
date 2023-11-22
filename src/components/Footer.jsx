9c44cdf4f8b736b55bf030f35d97ef305c865b73
Boudjenane Rachid <rboudjenane@jehann.fr>
2023-11-22 17:17:15 +0100

    feat(ui) style archi done :sparkles:

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Footer = () => {
+    const year = new Date().getFullYear()
+
+  return (
+    <footer className="footer p-10 bg-neutral text-neutral-content">
+    <nav>
+        <header className="footer-title text-2xl text-secondary">Dw-shop</header>
+        <p className="text-lg text-center font-bold">2013 - {year}</p>
+      </nav>
+      <nav>
+        <header className="footer-title">Services</header>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <header className="footer-title">Company</header>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <header className="footer-title">Legal</header>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ff00d1",
+          secondary: "#ff6b00",
+          accent: "#0053f0",
+          neutral: "#191b17",
+          "base-100": "#292929",
+          info: "#3b8fff",
+          success: "#008300",
+          warning: "#e63700",
+          error: "#dd002d",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
